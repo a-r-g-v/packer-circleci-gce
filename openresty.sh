109702b27b2d31c7db3ecc8cbce70f5544da07db
greyia <info@thun.ws>
2016-12-10 20:41:46 +0900

    wip

--- openresty.sh
+++ openresty.sh
@@ -3,12 +3,10 @@
 # openresty
 sudo wget https://github.com/cubicdaiya/nginx-build/releases/download/v0.9.10/nginx-build-linux-amd64-0.9.10.tar.gz
 sudo tar zxvf nginx-build-linux-amd64-0.9.10.tar.gz
-sudo apt-get install -y make gcc libluajit-5.1-2 luajit g++
+sudo apt-get install -y make gcc libluajit-5.1-2 luajit g++ libperl4-corelibs-perl
 export PATH=$PATH:/sbin
 sudo ./nginx-build  -openresty -pcre -openssl -zlib -d work
 cd work/openresty/1.11.2.1/openresty-1.11.2.1
 sudo make install
-sudo mkdir -p /etc/nginx
-sudo mv /tmp/nginx.service /etc/systemd/system/nginx.service
 sudo apt-get purge -y make gcc g++
 
